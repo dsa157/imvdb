056cde73ee8613b7de899fbb9634e1c5d8cca5f3
David April <dsa157@gmail.com>
2022-06-28 12:33:57 +0400

    added filter file for departments and ignore file for positions. added coding support for both

--- positions.pl
+++ positions.pl
@@ -4,15 +4,48 @@ use LWP::UserAgent;
 
 my %departments;
 my %positions;
-my @lines;
+my %ignore;
 
-open(F, "positions.csv");
-chomp(@lines = <F>);
-close F;
-getDepartments(@lines);
+getIgnoredPositions();
+getDepartments();
+#getPositions();
 
+#--------------------------------------------------------
+sub getIgnoredPositions{
+  my @lines;
+  open(F, "positions.ignore.csv");
+  chomp(@lines = <F>);
+  close F;
+  foreach(@lines) {
+    $ignore{$_}=1;
+  }
+}
+
+#--------------------------------------------------------
 sub getDepartments{
+  my @lines;
+  open(F, "departments.csv");
+  chomp(@lines = <F>);
+  close F;
   foreach(@lines) { 
+    my ($department, $include) = split(",", $_);
+    if ($include == 1) {
+      print "$department\n"; 
+      getPositions($department);
+    }
+  }
+}
+
+#--------------------------------------------------------
+sub getPositions{
+  my $thisDepartment = shift;
+  my @lines;
+  open(F2, "positions.csv");
+  chomp(@lines = <F2>);
+  close F2;
+  foreach(@lines) { 
+    next if (exists $ignore{$_});
+    next unless /^$thisDepartment/;
     my ($department, $position, $desc) = split(",", $_);
     next if $position eq "";
     #if not exists $departments{$department} {
@@ -20,13 +53,12 @@ sub getDepartments{
       #$departments{$department} = {};
     #}
     $departments{$department}{$position} = $desc;
+print "-- $position\n";
   }
   foreach (keys %departments) {
-    next unless /Editorial/;
-    my $tmp = $departments{"Editorial"};
-    foreach (keys %$tmp) {
-      print $_, "\n";
+    my $tmp = $departments{$_};
+    foreach (sort keys %$tmp) {
+      #print "  $_\n";
     }
   }
-  
 }
